5ec4c2cbb6eaeb6b7dfb571ce8e1982886633312
David Puddefoot <david-puddefoot@eastsideco.com>
2021-10-03 01:05:37 +0100

    updates readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dog-api
 Technical test for dog API
+
+# Task
+Use a simple API which returns breeds of dog and images as a JSON array. Use HTML, CSS and Javascript to read the API and display it in an organized way. In addition, it should contain some sort of sorting mechanic to the front-end. Freedom to use any frameworks/taskrunners/compilers to achieve this task.
+
+# Approch
+Using any library for this task seemed a bit overkill for a simple task. I deicded to got with an image gallery with essecially buttons which are actually just list items. Looking back I would probablly change this to buttons than list items to make them more accessible. The gallery will load with random images as I didnt want the page to be empty on load. this gallery will either show 15 random dogs on load or upto 15 of a selected breed. 
--- script.js
+++ script.js
@@ -1,72 +1,72 @@
 /*** API ***/
 // get all breeds from api
 async function getDogData() {
-  let url = 'https://dog.ceo/api/breeds/list/all';
+  let url = 'https://dog.ceo/api/breeds/list/all'
   
   try {
-    const response = await fetch(url);
-    const dogData = await response.json();
+    const response = await fetch(url)
+    const dogData = await response.json()
   
     // send the data to the list
-    renderDogsList(dogData.message);
+    renderDogsList(dogData.message)
   
   } catch (error) {
-      console.log(error);
+      console.log(error)
   }
 }
 
 // Breed list render fuction
 function renderDogsList(dogs) {
-  let html = '';
-  let dogList = document.querySelector('.dog-list');
+  let html = ''
+  let dogList = document.querySelector('.dog-list')
 
   // for each key in the object, add key value as list item
   for (const dog of Object.keys(dogs)) {
-    let htmlContent = `<li onclick="updateDogGallery(this.textContent)">${dog}</li>`;
-    html += htmlContent;
+    let htmlContent = `<li onclick="updateDogGallery(this.textContent)">${dog}</li>`
+    html += htmlContent
   }
   
   // add to dom
-  dogList.innerHTML = html;
+  dogList.innerHTML = html
 }
 
 async function updateDogGallery(dog) {
-  var imageUrl = '';
+  var imageUrl = ''
 
   // check if a breed has been selected
   if (dog){
     // if true fetch images for breed
-    imageUrl = `https://dog.ceo/api/breed/${dog}/images/random/15`;
+    imageUrl = `https://dog.ceo/api/breed/${dog}/images/random/15`
   } else {
     // if false fetch random images
-    imageUrl = 'https://dog.ceo/api/breeds/image/random/15';
+    imageUrl = 'https://dog.ceo/api/breeds/image/random/15'
   }
 
   try {
-    const response = await fetch(imageUrl);
-    const data = await response.json();
-    
+    const response = await fetch(imageUrl)
+    const data = await response.json()
+
     // update the gallery images
-    renderDogImages(data.message);
+    renderDogImages(data.message)
 
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
 
 function renderDogImages(dogImages) {
-  let html = '';
-  let dogGallery = document.querySelector('.dog-gallery');
+  let html = ''
+  let dogGallery = document.querySelector('.dog-gallery')
 
   // for each value in the object, add the value(url) as an image
   for (const img of Object.values(dogImages)) {
-    let htmlContent = `<picture><img src="${img}" loading="lazy"/></picture>`;
-    html += htmlContent;
+    let htmlContent = `<picture><img src="${img}" loading="lazy"/></picture>`
+    html += htmlContent
   }
 
   // add to dom
-  dogGallery.innerHTML = html;
+  dogGallery.innerHTML = html
 }
 
 getDogData()
